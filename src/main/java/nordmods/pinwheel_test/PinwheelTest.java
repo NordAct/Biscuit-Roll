252a764424e5cc8091293aeec1b19d2eec36f3aa
Nord_Act <73118477+NordAct@users.noreply.github.com>
2025-11-23 18:05:42 +0300

    init

---
+++ src/main/java/nordmods/pinwheel_test/PinwheelTest.java
@@ -0,0 +1,11 @@
+package nordmods.pinwheel_test;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PinwheelTest implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/nordmods/pinwheel_test/client/PinwheelTestClient.java
@@ -0,0 +1,10 @@
+package nordmods.pinwheel_test.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PinwheelTestClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
